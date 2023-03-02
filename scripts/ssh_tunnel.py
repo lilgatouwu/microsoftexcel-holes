b688d0b3048129af37ddae6ef30b355079b11409
Nolan Aatama <119261289+nolanaatama@users.noreply.github.com>
2023-03-02 16:34:00 +0700

    Add files via upload

--- scripts/ssh_tunnel.py
+++ scripts/ssh_tunnel.py
@@ -67,10 +67,15 @@ def ssh_tunnel(host: str = LOCALHOST_RUN) -> None:
     else:
         raise RuntimeError(f"Failed to run {host}")
 
+    # print(f" * Running on {tunnel_url}")
     os.environ['webui_url'] = tunnel_url
     colab_url = os.getenv('colab_url')
     strings.en["SHARE_LINK_MESSAGE"] = f"Public WebUI Colab URL: {tunnel_url}"
 
+
+def googleusercontent_tunnel():
+    colab_url = os.getenv('colab_url')
+    strings.en["SHARE_LINK_MESSAGE"] = f"WebUI Colab URL: {colab_url}"
 
 if cmd_opts.localhostrun:
     print("localhost.run detected, trying to connect...")
@@ -79,3 +84,7 @@ if cmd_opts.localhostrun:
 if cmd_opts.remotemoe:
     print("remote.moe detected, trying to connect...")
     ssh_tunnel(REMOTE_MOE)
+
+if cmd_opts.googleusercontent:
+    print("googleusercontent.com detected, trying to connect...")
+    googleusercontent_tunnel()
